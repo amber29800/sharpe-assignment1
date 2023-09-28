73c58fb506650cb7640287dd0db5c0b43aa98998
Amber Srivastava <ambersrivastava19@gmail.com>
2023-09-28 17:45:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { useEffect } from "react";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 function App() {
+  const [chartData, setChartData] = useState();
+
+  useEffect(() => {
+    getChartData();
+  }, []);
+
+  const getChartData = async () => {
+    const data = await fetch(
+      "https://api.llama.fi/summary/fees/lyra?dataType=dailyFees"
+    );
+    const json = await data.json();
+    const dataInJson = getDataInJSON(json.totalDataChart);
+    setChartData(dataInJson);
+  };
+
+  const getDataInJSON = (dataInArray) => {
+    const convertedData = [];
+
+    for (const innerArray of dataInArray) {
+      const timestamp = new Date(innerArray[0] * 1000); // Convert to milliseconds
+      const month = timestamp.toLocaleString("default", { month: "long" });
+      const day = timestamp.getDate();
+      const year = timestamp.getFullYear();
+      const jsonObject = {
+        date: `${month} ${day}, ${year}`,
+        value: innerArray[1],
+      };
+      convertedData.push(jsonObject);
+    }
+    const mainJSON = {
+      totalDataChart: convertedData,
+    };
+    return mainJSON.totalDataChart;
+  };
+
+  console.log(chartData);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <AreaChart
+          width={1450}
+          height={720}
+          data={chartData}
         >
-          Learn React
-        </a>
-      </header>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Area
+            type="monotone"
+            dataKey="value"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+        </AreaChart>
     </div>
   );
 }
